ce7b6ea106411efc3018ede7c03ac8cabb9478ce
ntpit98 <ntpit98@gmail.com>
2026-04-17 13:19:40 +0700

    Update connect EC2 to RDS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started Deploy EC2 USE DOCKER</h1>
+          <h1>Update connect EC2 to RDS</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
